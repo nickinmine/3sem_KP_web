25c6b9207b000d3a750ead2fbff33d5d4d81e8da
Nick <nickinmine@gmail.com>
2021-11-15 23:26:52 +0300

    added pages and others mods

--- jscript/script.js
+++ jscript/script.js
@@ -9,3 +9,37 @@ menuBurger.addEventListener("click", function (e) {
     headerLocker.classList.toggle('lock');
     menuBody.classList.toggle('active');
 });
+
+//content animation
+const animItems = document.querySelectorAll('.content-animation');
+if (animItems.length > 0) {
+    window.addEventListener('scroll', animateContent);
+    function animateContent() {
+        for (let i = 0; i < animItems.length; i++) {
+            const animItem = animItems[i];
+            const animItemHeight = animItem.offsetHeight;
+            const animItemOffset = offset(animItem).top;
+            const animStart = 3;
+            let animItemPoint = window.innerHeight - animItemHeight / animStart;
+            if (animItemHeight > window.innerHeight) {
+                animItemPoint = window.innerHeight - window.innerHeight / animStart;
+            }
+            if ((pageYOffset > animItemOffset - animItemPoint) &&
+                pageYOffset < (animItemOffset + animItemHeight) &&
+                !animItem.classList.contains('visible')) {
+                animItem.classList.add('visible');
+            } //else { animItem.classList.remove('visible'); }
+        }
+    }
+    function offset(element) {
+        const rect = element.getBoundingClientRect();
+        const scrollLeft = window.pageXOffset || document.documentElement.scrollLeft;
+        const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+        return {
+            top: rect.top + scrollTop, left: rect.left + scrollLeft
+        }
+    }
+    setTimeout(() => {
+        animateContent();
+    }, 150);
+}
